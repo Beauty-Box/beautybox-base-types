eee9a871f2a41c38d6b00e493254b15e2dc158a3
Brix-D <Shlyakhta4@gmail.com>
2023-04-24 16:48:26 +0400

    feat(helpers): added function deep equal from vuetify

---
+++ helpers/deepEqual.ts
@@ -0,0 +1,4 @@
+declare function deepEqual(a: any, b: any): boolean;
+
+export { deepEqual };
+export default deepEqual;
--- helpers/index.d.ts
+++ helpers/index.d.ts
@@ -2,4 +2,5 @@ export * from './objectToURLParams';
 export * from './convertMinutesToHours';
 export * from './declensions';
 export * from './isNumber';
+export * from './deepEqual';
 export * from './genArrayTimeIntervalMin';
