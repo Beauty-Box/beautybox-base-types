e2a672f29127a213a767fe656617df1f1b222044
Brix-D <Shlyakhta4@gmail.com>
2023-04-26 16:45:10 +0400

    types

--- utils/validators.d.ts
+++ utils/validators.d.ts
@@ -1,20 +1,25 @@
 import { helpers } from '@vuelidate/validators';
 
-declare interface validatorName {
+ interface IValidatorName {
     required: ReturnType<typeof helpers.withMessage>;
     minLength: ReturnType<typeof helpers.withMessage>;
     maxLength: ReturnType<typeof helpers.withMessage>;
 }
 
-declare interface validatorPhone {
+ interface IValidatorPhone {
     required: ReturnType<typeof helpers.withMessage>;
     minLength: ReturnType<typeof helpers.withMessage>;
 }
 
-declare interface validatorPassword {
+ interface IValidatorPassword {
     required: ReturnType<typeof helpers.withMessage>;
     minLength: ReturnType<typeof helpers.withMessage>;
     maxLength: ReturnType<typeof helpers.withMessage>;
 }
+
+declare const validatorName: IValidatorName;
+declare const validatorPhone: IValidatorPhone;
+declare const validatorPassword: IValidatorPassword;
+
 
 export { validatorName, validatorPhone, validatorPassword };
