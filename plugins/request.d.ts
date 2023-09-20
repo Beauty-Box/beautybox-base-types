b54642a4ba7834ed82ea4d48350d929fa7832453
Brix-D <Shlyakhta4@gmail.com>
2023-09-20 15:14:53 +0400

    fix(request types): try to type request plugin

--- plugins/request.d.ts
+++ plugins/request.d.ts
@@ -2,4 +2,10 @@ import { Api } from '../api';
 
 declare function useApi(): { provider: Api };
 
+declare const request: {
+    install(app: any, ...options: unknown[]): any;
+}
+
 export { useApi };
+
+export default request;
